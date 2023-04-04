d124d2f3dc6ec00daa283aa58757d8f8242bd1bd
Or Rodrigez <orrodrigez1@gmail.com>
2023-04-04 16:41:58 +0300

    Finish the game logic

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,34 +6,39 @@ import '../styles/main.css'
 const Main = () => {
 
   const [flags, setFlags] = useState(flagsObject)
+  const [score, setScore] = useState(0)
+  const [bestScore, setBestScore] = useState(0)
 
   const handleClick = (e) => {
     // eslint-disable-next-line
     const clickedFlag = flags.find(flag => flag.id == e.target.id)
     // if the flag isn't clicked
     if(!clickedFlag.clicked) {
-      // mark as clicked
+      // mark as clicked and update score
       clickedFlag.clicked = true
+      setScore(score + 1)
       // then check if all flags are clicked
       let allFlagsClicked = flags.every(flag => flag.clicked)
       if(allFlagsClicked) {
         resetGame()
-        console.log('you win')
+        alert('You win')
         return
       }
       // if flag is already clicked
     } else {
       resetGame()
-      console.log('game over')
     }
     shuffle()
   }
 
   const resetGame = () => {
+    if(score > bestScore) setBestScore(score)
     // reset all flags
     flags.forEach(flag => {
       flag.clicked = false
     })
+    // set score to 0
+    setScore(0)
   }
 
   const shuffle = () => {
@@ -54,10 +59,23 @@ const Main = () => {
 
   return (
     <div className="main">
+      <div className="scores">
+        <h2>Score: {score}</h2>
+        <h2>Best Score: {bestScore}</h2>
+      </div>
       <div className="cards-container">
         {flags.map(flag => {
           return <Card src={flag.img} cardName={flag.flagName} onClick={handleClick} id={flag.id} key={flag.id}/>
         })}
+      </div>
+      <div className="rules">
+        <h1>How to play</h1>
+        <h3>
+          You may only click on each flag once.<br/>
+          After each click, the cards will shuffle, and you have to remember what you've already clicked.<br/>
+          Clicking the same card twice will reset your score.<br/>
+          Reach a score of 8 to win.
+        </h3>
       </div>
     </div>
   )
