8c8502483d80bfdb5bb8942790aed50fdd5a682c
Or Rodrigez <orrodrigez1@gmail.com>
2023-04-04 14:30:18 +0300

    Prevent dragging images

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import '../styles/card.css';
 const Card = ({src, cardName, onClick}) => {
   return (
     <div className="card" onClick={onClick}>
-      <img src={src} alt={cardName} />
+      <img src={src} alt={cardName} draggable={false}/>
       <div className="text-container">
         <h2>{cardName}</h2>
       </div>
